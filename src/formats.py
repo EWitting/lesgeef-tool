cb72bc8ae4f4f5b2e1dfea94b13c5051ab4fce66
Emiel <emiel.witting@gmail.com>
2025-09-06 20:54:54 +0200

    finish formatting

--- src/formats.py
+++
@@ -1,9 +0,0 @@
-import xlsxwriter
-
-def add_formats(workbook: xlsxwriter.workbook) -> dict[str, xlsxwriter.format]:
-
-    return {
-        "header": workbook.add_format({"align": "center", "bold": True}),
-        "center": workbook.add_format({"align": "center", "valign": "vcenter"}),
-        "top-left": workbook.add_format({"align": "left", "valign": "top"}),
-    }
